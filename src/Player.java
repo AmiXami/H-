67d015b9496d5416cafdbf599cb7326483969eee
AmiXami <xami2503Gmail.com>
2019-08-06 22:53:41 -1200

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,34 @@
+    public class  Player {
+    private int Hp;
+    private int Damage;
+    private String Attributes;
+
+
+
+
+
+
+        public int getHp() {
+            return Hp;
+        }
+
+        public void setHp(int hp) {
+            Hp = hp;
+        }
+
+        public int getDamage() {
+            return Damage;
+        }
+
+        public void setDamage(int damage) {
+            Damage = damage;
+        }
+
+        public String getAttributes() {
+            return Attributes;
+        }
+
+        public void setAttributes(String attributes) {
+            Attributes = attributes;
+        }
+    }
